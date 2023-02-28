d87e82bc39f2f88cb841487314ef4fda2f9c55b7
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-02-28 10:42:02 +0100

    update test_stack

--- Stack.py
+++ Stack.py
@@ -15,7 +15,21 @@ class Stack():
 
 def test_stack():
   s = Stack()
-  print('all tests passed')
+  
+  #is_stack_empty test.
 
   #Check if Stack is succesfuly created.
   assert s != None, f'''The instance is not created successfully'''
+  
+  #Check if is_stack_empty works.
+  s.is_stack_empty()
+  assert s == False or s==True, f'Is_stack_empty must return True or False'
+
+  #push test.
+  s.push(x=4)
+
+  #Check if push takes an integer.
+  if not isinstance(x, int):
+    raise ValueError(f'x must be int.')
+
+  print('all tests passed')
