675c966862237516c4bb8e85e6c286aaae17cffe
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-03-01 12:03:20 +0100

    syntax fix

--- Stack.py
+++ Stack.py
@@ -18,21 +18,21 @@ def test_stack():
   s = Stack()
   
   #Check if Stack is succesfuly created.
-  assert s != None, f'''The instance is not created successfully.'''
+  assert s != None, f'The instance is not created successfully.'
 
   #Ckeck if s is empty.
-  assert s.is_stack_empty(), f'''At this point stack must be empty.'''
+  assert s.is_stack_empty(), f'At this point stack must be empty.'
 
-  s_push_item_1 = s.push(x=4)
-  s_push_item_2 = s.push(x='letter') 
+  s.push(x=4)
+  s.push(x='letter') 
 
   assert s.is_stack_empty() == False, f' Stack must not be empty at this point. '
   
   #Check if pop remove items correctly.
   s_pop_element = s.pop() 
-  assert s_pop == s_push_element_2, f'Unespected output. '
+  assert s_pop == 'letter', f'Unespected output. '
   s_pop_element = s.pop() 
-  assert s_pop == s_push_element_1, f'Unespected output. '
+  assert s_pop == '4', f'Unespected output. '
 
   assert s.is_stack_empty() == True, f'At this point stack must be empty.'
 
