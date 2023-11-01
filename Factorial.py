06d626a122c78079ec11f96c68a7cc0a4ddc08a5
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-11-01 19:08:46 +0100

    Delete Factorial.py

--- Factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1  # Base case
-    else:
-        return n * factorial(n - 1)  # Recursive case
-
-result = factorial(5)  # Calculates 5! = 5 * 4 * 3 * 2 * 1 = 120
-print(result)  # Output: 120
